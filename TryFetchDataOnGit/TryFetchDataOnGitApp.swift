56d9d0f25b565eb8b6abd88a88e11bbc5542f453
Dmitry Gerashchenko <finiqw@gmail.com>
2021-06-30 03:17:29 +0700

    Initial Commit

---
+++ TryFetchDataOnGit/TryFetchDataOnGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryFetchDataOnGitApp.swift
+//  TryFetchDataOnGit
+//
+//  Created by Дмитрий Геращенко on 30.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct TryFetchDataOnGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
