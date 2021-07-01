9e359fe172448e1cffdd1f3d1280f1567a3e9c1f
Dmitry Gerashchenko <finiqw@gmail.com>
2021-07-01 18:25:58 +0700

    minimal version

--- TryFetchDataOnGit/ContentView.swift
+++ TryFetchDataOnGit/ContentView.swift
@@ -1,21 +1,58 @@
-//
-//  ContentView.swift
-//  TryFetchDataOnGit
-//
-//  Created by Дмитрий Геращенко on 30.06.2021.
-//
-
 import SwiftUI
 
+struct Response: Codable {
+  var items: [Result]
+}
+
+struct Result: Codable {
+  var id: Int
+  var name: String
+//  var collectionName: String
+}
+
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  
+  @State private var results = [Result]()
+  
+  var body: some View {
+    List(results, id: \.id) { item in
+      VStack(alignment: .leading) {
+        Text(item.name)
+          .font(.headline)
+//        Text(item.collectionName)
+//          .foregroundColor(.secondary)
+      }
     }
+    .onAppear {
+      loadData()
+    }
+    
+  }
+  func loadData() {
+    guard let url = URL(string: "https://api.github.com/search/repositories?q=Star+Wars") else {
+      print("Invalid URL")
+      return
+    }
+    
+    let request = URLRequest(url: url)
+    
+    URLSession.shared.dataTask(with: request) { data, response, error in
+      if let data = data {
+        if let results = try? JSONDecoder().decode(Response.self, from: data) {
+          DispatchQueue.main.async {
+            self.results = results.items
+          }
+          return
+        }
+      }
+      print("Error: \(error?.localizedDescription ?? "Unknown error")")
+    }
+    .resume()
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
---
+++ TryFetchDataOnGit/Service.swift
@@ -0,0 +1,24 @@
+//
+//  Service.swift
+//  TryFetchDataOnGit
+//
+//  Created by Дмитрий Геращенко on 30.06.2021.
+//
+
+import Foundation
+
+//struct Response: Codable {
+//  var results: [Item]
+//}
+//
+//struct Item: Codable {
+//  var id: Int?
+//  var name: String?
+//  var language: String?
+//  var description: String
+//  var stargazers_count: Int
+//  var forks: Int
+//  var license: License
+//  var owner: Owner
+// 
+//}
